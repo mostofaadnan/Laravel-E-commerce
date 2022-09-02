2e11c85e58e7d934cf355532a45e67cba59ac613
Mostofa Shahadat Adnan <msa.cse.metro@gmail.com>
2022-09-02 14:06:21 +0600

    Delete database directory

--- database/seeds/barcode_configarations.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO `barcode_configarations` (`id`, `companyshow`, `itemcodeshow`, `itemnameshow`, `itempriceshow`, `itemothernoteshow`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 1, 1, 1, NULL, '2020-09-24 21:56:19');
-
